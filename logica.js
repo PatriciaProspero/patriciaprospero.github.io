d9bcc318725a0ba85483e7b5173cde8e91c2ea29
patricia <patricia.prospero@hotmail.com>
2018-01-14 17:29:44 -0200

    Adicionando Arquivos

---
+++ logica.js
@@ -0,0 +1,18 @@
+
+	function soma (n1,n2)
+	{
+		document.write (n1+n2);
+	}
+	
+
+function tabuada (numeroRecebido) 
+{
+	document.write("Numero recebido: " + numeroRecebido);
+	document.write("<br>");
+
+for( i = 1; i < 10 ; i++)
+	{
+		document.write(numeroRecebido + "x" + i +" = " + numeroRecebido*i);
+		document.write ("<br>");
+	}
+}
